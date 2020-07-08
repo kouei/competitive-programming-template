142dbedd787b2eda745c479bcdcad6d56b4a613b
Kouei <kagigokoro@gmail.com>
2020-07-08 16:15:15 +0800

    truncate main.cpp & input.txt

--- main.cpp
+++ main.cpp
@@ -2,15 +2,5 @@
 using namespace std;
 
 int main() { 
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for(int & i : v) {
-        cin >> i;
-    }
-    for(int i : v) {
-        cout << i << " ";
-    }
-    cout << "\n";
     return 0;
-} 
+}
