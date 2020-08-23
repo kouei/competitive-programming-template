6dc481b92675693a837c3bd82daa680ae01d6ef5
kouei <kagigokoro@gmail.com>
2020-08-23 18:03:44 +0800

    Add I/O accelerator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+static int _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+
+
+
+
+
 int main() { 
     int T;
     cin >> T;
