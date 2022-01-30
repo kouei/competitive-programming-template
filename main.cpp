6ab35fa12de472a249a210f8b0a9af32f6bc1937
kouei <kagigokoro@gmail.com>
2022-01-31 01:14:45 +0800

    Add pb_ds

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+/**************************Header Part Begin**************************/
 #include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+//using namespace __gnu_pbds;
 
 static int _ = [](){
     ios_base::sync_with_stdio(false);
@@ -7,13 +12,12 @@ static int _ = [](){
     cout.tie(nullptr);
     return 0;
 }();
+/***************************Header Part End***************************/
 
 
 
 
-
-
-int main() { 
+int main() {
     int T;
     cin >> T;
     for(auto tc = 1; tc <= T; ++tc) {
