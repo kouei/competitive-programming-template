708a26d71fc2c1ceb209e48234471f3854b4bac7
kouei <kagigokoro@gmail.com>
2022-02-20 20:20:28 +0800

    Add template for tree order statistics

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
-/**************************Example Part Begin**************************/
-// pb_ds tree_order_statistics example
-// https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/testsuite/ext/pb_ds/example/tree_order_statistics.cc
-/***************************Example Part End***************************/
-
-/**************************Header Part Begin**************************/
 #include<bits/stdc++.h>
-//#include <ext/pb_ds/assoc_container.hpp>
-//#include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-//using namespace __gnu_pbds;
 
 static int _ = [](){
     ios_base::sync_with_stdio(false);
@@ -17,18 +7,12 @@ static int _ = [](){
     cout.tie(nullptr);
     return 0;
 }();
-/***************************Header Part End***************************/
-
-
-
 
 int main() {
     int T;
     cin >> T;
     for(auto tc = 1; tc <= T; ++tc) {
-
-        
-        
+        // TODO: Add your code here.
         cout << "Case #" << tc << ": ";
     }
     return 0;
