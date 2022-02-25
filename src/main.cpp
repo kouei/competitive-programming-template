a62b954c4cb3d90ae1aa89c4061ce14ba0ed9faf
kouei <kagigokoro@gmail.com>
2022-02-26 03:02:32 +0800

    Partially implemented SA-IS suffix array construction algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,55 @@ static int _ = [](){
     return 0;
 }();
 
+/// <summary>
+/// Create suffix array for str using SA-IS algorithm.
+/// </summary>
+/// <param name="s">Input string, each character is represented as a number in {1..K}. </param>
+/// <param name="sa">An existing array to place the suffix array.</param>
+/// <param name="K">Maximum character in the dictionary of str. The dictionary is represented as {1..K}.</param>
+void createSuffixArray(vector<int> & s, vector<int> & sa, int K) {
+    int n = s.size();
+    s.push_back(0); // Add the smallest character in dictionary to the end of string.
+
+    const int S_TYPE = 0;
+    const int L_TYPE = 1;
+    vector<int> t(n + 1);
+
+    t[n] = S_TYPE;
+    for(int i = n - 1; i >= 0; --i) {
+        if(s[i] < s[i + 1]) {
+            t[i] = S_TYPE;
+        } else if(s[i] > s[i + 1]) {
+            t[i] = L_TYPE;
+        } else {
+            t[i] = t[i + 1];
+        }
+    }
+
+    vector<int> p1;
+    p1.reserve(n + 1);
+
+    for(int i = 1; i < n; ++i) {
+        if(t[i] == S_TYPE && t[i - 1] == L_TYPE) {
+            p1.push_back(i);
+        }
+    }
+    p1.push_back(n);
+
+    vector<int> l_count_for_each_bucket(K + 1);
+    for(int i = 0; i < n + 1; ++i) {
+        if(t[i] == L_TYPE) {
+            l_count_for_each_bucket[s[i]] += 1;
+        }
+    }
+}
+
 int main() {
-    int T;
-    cin >> T;
-    for(auto tc = 1; tc <= T; ++tc) {
-        // TODO: Add your code here.
-        cout << "Case #" << tc << ": ";
+    string str = "ABRACADABRA";
+    int n = str.size();
+    vector<int> s(n);
+    for(int i = 0; i < n; ++i) {
+        s[i] = str[i] - 'A' + 1;
     }
     return 0;
 }
