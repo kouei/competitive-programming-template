95bad6e99d253fd8ae2b4d21e52c29306a690a76
kouei <kagigokoro@gmail.com>
2024-07-07 01:49:28 +0800

    Implement test

--- test/src/step0-gen.cpp
+++ test/src/step0-gen.cpp
@@ -10,16 +10,10 @@ int rand(int start, int end) {
 }
 
 int main(int argc, char* argv[]) {
-    int n = rand(2, 30);
-    printf("%d\n", n);
-    set<int> used;
+    int n = 5;
     for(int i = 0; i < n; ++i) {
-        int x;
-        do {
-            x = rand(1, 30);
-        } while(used.count(x));
+        int x = rand(1, 10);
         printf("%d ", x);
-        used.insert(x);
     }
     puts("");
 }
