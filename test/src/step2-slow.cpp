6af26afe2c3c4353127d2d02d59318b468664e3f
kouei <kagigokoro@gmail.com>
2024-07-07 02:46:42 +0800

    Update slow

--- test/src/step2-slow.cpp
+++ test/src/step2-slow.cpp
@@ -12,6 +12,7 @@ static int _ = [](){
 }();
 
 struct Monster {
+    int max_hp;
     int hp;
     bool is_aoe_triggered;
 
@@ -69,92 +70,55 @@ bool trigger_aoe(vector<Monster> & monsters, bool is_print = false) {
     return false;
 }
 
-tuple<int, vector<int>> dfs() {
+int solve() {
+
+    vector<int> index;
+    for(int i = 0; i < (int)init_monters.size(); ++i) {
+        index.push_back(i);
+    }
 
     int min_step = INT_MAX;
-    vector<int> min_history;
+    do {
+        int step = 0;
+        auto monsters = init_monters;
 
-    vector<tuple<int, vector<int>, vector<Monster>>> stk;
-    stk.emplace_back(0, vector<int>{}, init_monters);
-
-    while(!stk.empty()) {
-        auto step = move(get<0>(stk.back()));
-        auto history = move(get<1>(stk.back()));
-        auto monsters = move(get<2>(stk.back()));
-        stk.pop_back();
-
-        if(is_all_monsters_dead(monsters)) {
-            if (min_step > step) {
-                min_step = step;
-                min_history = move(history);
-            }
-
-            continue;
-        }
-
-        for(size_t i = 0; i < monsters.size(); ++i) {
-            if (monsters[i].hp <= 0) {
+        for(int i : index) {
+            if(monsters[i].is_aoe_triggered) {
                 continue;
             }
 
-            auto new_monsters = monsters;
-            new_monsters[i].decrease_hp();
-            while(trigger_aoe(new_monsters)) {}
+            if(monsters[i].hp <= monsters[i].max_hp / 2) {
+                continue;
+            }
 
-            auto new_history = history;
-            new_history.emplace_back(i);
-            stk.emplace_back(step + 1, move(new_history), move(new_monsters));
+            step += monsters[i].hp - monsters[i].max_hp / 2;
+            monsters[i].hp = monsters[i].max_hp / 2;
+
+            while(trigger_aoe(monsters)) {}
         }
-    }
 
-    return { min_step, min_history };
+        for(const auto & m : monsters) {
+            step += m.hp;
+        }
+
+        min_step = min(min_step, step);
+    } while(next_permutation(index.begin(), index.end()));
+
+    return min_step;
 }
 
 int main() {
     while (true) {
         int hp;
         if (cin >> hp) {
-            init_monters.push_back({hp, false});
+            init_monters.push_back({hp, hp, false});
         } else {
             break;
         }
     }
 
-    auto result = dfs();
-    auto min_step = get<0>(result);
-    auto min_history = move(get<1>(result));
-
-#if false
-    cout << "Min Step = " << min_step << "\n\n";
-
-    cout << "Init Monsters:" << "\n";
-    print_monsters(init_monters);
-    cout << "\n";
-
-    auto monsters = init_monters;
-    for (size_t i = 0; i < min_history.size(); ++i) {
-        cout << "Step " << i + 1 << ":" << "\n";
-
-        int pos = min_history[i];
-        print_monsters(monsters, pos);
-        
-
-        monsters[pos].decrease_hp();
-
-        if(trigger_aoe(monsters, true)) {
-            print_monsters(monsters);
-            while(trigger_aoe(monsters, true)) {
-                print_monsters(monsters);
-            }
-        } else {
-            print_monsters(monsters);
-        }
-
-        cout << "\n";
-    }
-#else
+    int min_step = solve();
     cout << min_step << "\n\n";
-#endif
-
+    
     return 0;
 }
