4a25649bb234025117142724b33faebf7dc9fe09
kouei <kagigokoro@gmail.com>
2024-07-07 01:47:18 +0800

    Implement fast

--- test/src/step1-fast.cpp
+++ test/src/step1-fast.cpp
@@ -1,16 +1,119 @@
-// incorrect solution for finding second smallest element
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-// #warning TODO: remember about bigger N
-const int MAX_N = 1e6 + 5;
-int a[MAX_N];
-set<int> asd[MAX_N], asdfasd[3*MAX_N];
-int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i = 1; i <= n; ++i) {
-        scanf("%d", &a[i]);
+
+#pragma GCC optimize("O2,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+static int _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+struct Monster {
+    int hp;
+    bool is_aoe_triggered;
+
+    void decrease_hp() {
+        hp = max(0, hp - 1);
     }
-    sort(a + 1, a + n);
-    printf("%d\n", a[2]);
+
+    bool operator< (const Monster & rhs) const { return hp < rhs.hp; }
+};
+
+vector<Monster> init_monters;
+
+bool is_all_monsters_dead(const vector<Monster> & monsters) {
+    for(auto m : monsters) {
+        if(m.hp > 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void print_monsters(const vector<Monster> & monsters, int pos = -1, char left = '[', char right = ']') {
+    for(int i = 0; i < (int)monsters.size(); ++i) {
+        if(i == pos) {
+            cout << left << monsters[i].hp << right << " ";
+        } else {
+            cout << monsters[i].hp << " ";
+        }
+    }
+
+    cout << "\n";
+}
+
+bool trigger_aoe(vector<Monster> & monsters, bool is_print = false) {
+    for(size_t i = 0; i < monsters.size(); ++i) {
+        if (monsters[i].is_aoe_triggered) {
+            continue;
+        }
+
+        if (monsters[i].hp > init_monters[i].hp / 2) {
+            continue;
+        }
+
+        if(is_print) {
+            print_monsters(monsters, (int)i, '(', ')');
+        }
+
+        monsters[i].is_aoe_triggered = true;
+
+        for(auto & m : monsters) m.decrease_hp();
+
+
+        return true;
+    }
+
+    return false;
+}
+
+int solve() {
+    int n = (int)init_monters.size();
+    auto monsters = init_monters;
+    
+    int min_step = 0;
+    for(auto & m : monsters) {
+        if (m.hp > n) {
+            min_step += m.hp - n;
+            m.hp = n;
+        }
+    }
+
+    while(trigger_aoe(monsters)) {}
+
+    sort(monsters.begin(), monsters.end());
+
+    for(size_t i = 0; i < monsters.size(); ++i) {
+        if(monsters[i].hp > init_monters[i].hp / 2) {
+            min_step += monsters[i].hp - init_monters[i].hp / 2;
+            monsters[i].hp = init_monters[i].hp / 2;
+        }
+
+        while(trigger_aoe(monsters)) {}
+    }
+
+    for(auto & m : monsters) {
+        min_step += m.hp;
+    }
+
+    return min_step;
+}
+
+int main() {
+    while (true) {
+        int hp;
+        if (cin >> hp) {
+            init_monters.push_back({hp, false});
+        } else {
+            break;
+        }
+    }
+
+    int min_step = solve();
+    cout << "Min Step = " << min_step << "\n\n";
+    return 0;
 }
